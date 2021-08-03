6fe5c7f59f2a2d3667dfe6860ab4b68292c8d551
Roger Pimenta <roger.pimenta@live.com>
2021-08-03 17:38:15 -0300

    Finally

--- src/main/java/com/rdpimenta/course/resources/UserResource.java
+++ src/main/java/com/rdpimenta/course/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 
 	@GetMapping
 	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Joao", "joaooo0@gmail.com", "99999", "12345");
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999", "12345");
 		
 		return ResponseEntity.ok().body(u);
 	}
