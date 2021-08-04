98a3e155fdacb9a76d22b245e5173d92a91b626e
Roger Pimenta <roger.pimenta@live.com>
2021-08-04 15:13:29 -0300

    Entidade Product

---
+++ src/main/java/com/rdpimenta/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.rdpimenta.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rdpimenta.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
