9ca80b3825354cc155cf80ab9017c94891bd21f5
Roger Pimenta <roger.pimenta@live.com>
2021-08-05 14:44:06 -0300

    Entidade OrderItem, associação muitos-pra-muitos com dados extras

---
+++ src/main/java/com/rdpimenta/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.rdpimenta.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rdpimenta.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
