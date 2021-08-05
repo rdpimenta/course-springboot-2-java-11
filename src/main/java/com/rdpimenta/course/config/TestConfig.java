2b815204f47f90844ab56e43cdc12e75cc9b2787
Roger Pimenta <roger.pimenta@live.com>
2021-08-05 16:11:12 -0300

    Entidade payment, associação um para um

--- src/main/java/com/rdpimenta/course/config/TestConfig.java
+++ src/main/java/com/rdpimenta/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rdpimenta.course.entities.Category;
 import com.rdpimenta.course.entities.Order;
 import com.rdpimenta.course.entities.OrderItem;
+import com.rdpimenta.course.entities.Payment;
 import com.rdpimenta.course.entities.Product;
 import com.rdpimenta.course.entities.User;
 import com.rdpimenta.course.entities.enums.OrderStatus;
@@ -77,5 +78,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/com/rdpimenta/course/entities/Order.java
+++ src/main/java/com/rdpimenta/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,9 +13,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.rdpimenta.course.entities.enums.OrderStatus;
 
 @Entity
@@ -37,6 +40,9 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 	}
@@ -85,6 +91,14 @@ public class Order implements Serializable {
 	
 	public Set<OrderItem> getOrderItem() {
 		return items;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override
