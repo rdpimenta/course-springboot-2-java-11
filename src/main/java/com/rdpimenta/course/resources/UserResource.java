971f6ff76c251d8dcf5a5546c5ed6031ea585f92
Roger Pimenta <roger.pimenta@live.com>
2021-08-06 11:04:15 -0300

    Inserção de usuário

--- src/main/java/com/rdpimenta/course/resources/UserResource.java
+++ src/main/java/com/rdpimenta/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.rdpimenta.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rdpimenta.course.entities.User;
 import com.rdpimenta.course.services.UserService;
@@ -31,5 +35,13 @@ public class UserResource {
 		User user = service.findById(id);
 		
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
+		service.saveUser(user);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
 	}
 }
--- src/main/java/com/rdpimenta/course/services/UserService.java
+++ src/main/java/com/rdpimenta/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User saveUser(User user) {
+		return repository.save(user);
+	}
 }
