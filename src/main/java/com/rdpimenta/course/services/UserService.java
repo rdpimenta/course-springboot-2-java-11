a17e8c23eecbac1fb0db25fe0d6914d2a05e1367
Roger Pimenta <roger.pimenta@live.com>
2021-08-06 14:31:07 -0300

    Tratamento de exceção - delete

--- src/main/java/com/rdpimenta/course/services/UserService.java
+++ src/main/java/com/rdpimenta/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.rdpimenta.course.entities.User;
 import com.rdpimenta.course.repositories.UserRepository;
+import com.rdpimenta.course.services.excepetions.DatabaseException;
 import com.rdpimenta.course.services.excepetions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void deleteUserById(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User user) {
---
+++ src/main/java/com/rdpimenta/course/services/excepetions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.rdpimenta.course.services.excepetions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
