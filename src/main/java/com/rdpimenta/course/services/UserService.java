300653d5b888ccffba9f84c929c6c065f06d10bb
Roger Pimenta <roger.pimenta@live.com>
2021-08-06 14:16:52 -0300

    Tratamento de exceção - findById

--- src/main/java/com/rdpimenta/course/services/UserService.java
+++ src/main/java/com/rdpimenta/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.rdpimenta.course.entities.User;
 import com.rdpimenta.course.repositories.UserRepository;
+import com.rdpimenta.course.services.excepetions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User saveUser(User user) {
